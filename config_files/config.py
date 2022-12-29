a82b628f824850f25f1011225643ad26a5198a54
shivpalSW <shivpal.wadkar2050@gmail.com>
2022-12-29 21:36:40 +0530

    file updated

---
+++ config_files/config.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_PATH = os.getcwd()
+
+MODEL_PATH = os.path.join(BASE_PATH, r'artifacts\Advance_house_price.pkl')
+
+CSV_PATH = os.path.join(BASE_PATH, r'CSV_Files\Ames_Housing_Data.csv')
+
+PORT_NUMBER = 5001
+
